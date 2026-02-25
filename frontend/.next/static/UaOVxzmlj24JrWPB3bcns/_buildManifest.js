0de1627d39bbecae9969460ec88dca9d282fb3b3
NandiniJerripothula-14 <jerripothulanandini@gmail.com>
2026-02-25 22:09:16 +0530

    chore: remove generated artifacts from repository

--- frontend/.next/static/UaOVxzmlj24JrWPB3bcns/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/573-c7f1e640f7c98a41.js","static/chunks/pages/index-28120e2321aee740.js"],"/_error":["static/chunks/pages/_error-7a92967bea80186d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
